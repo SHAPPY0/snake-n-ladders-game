9df861e53e4e182c9d7eeec0edaed1aee089a8a2
saurabh <sharmasaurabh450@gmail.com>
2024-02-12 09:10:01 +0530

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # snake-n-ladders-game
+
+### Load The Game:
+```python
+    board_size = 100
+    dice_num = 1
+    game = SnakeLadder(board_size, dice_num)
+
+    #Add Ladders
+    game.ladders.Add(1, 38)
+    game.ladders.Add(4, 14)
+    game.ladders.Add(28, 84)
+
+    #Add Snakes
+    game.snakes.Add(17, 7)
+    game.snakes.Add(54, 34)
+    game.snakes.Add(62, 19)
+    game.snakes.Add(93, 73)
+
+    #Add Players
+    game.AddPlayer("Abc")
+    game.AddPlayer("Xyz")
+    
+```
+
+### Start The Game:
+
+```python
+    game.Start()
+
+    #Roll The Dice:
+    In CLI it will show player name based on the turns, press 'r' key and press Enter.
+```
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from turns import Turns
 from jumper import Ladders, Snakes
 
 class SnakeLadder:
-    def __init__(self, boardSize, diceNum):
+    def __init__(self, board_size, dice_num):
         self.players = []
         self.truns = Turns()
-        self.dice = Dice(diceNum)
-        self.boardSize = boardSize
-        self.board = [n for n in range(1, self.boardSize + 1)]
-        self.playerPositions = {}
+        self.dice = Dice(dice_num)
+        self.board_size = board_size
+        self.board = [n for n in range(1, self.board_size + 1)]
+        self.player_positions = {}
         self.ladders = Ladders()
         self.snakes = Snakes()
 
@@ -20,42 +20,42 @@ class SnakeLadder:
     def Start(self):
         for player in self.players:
             self.truns.put(player)
-            self.playerPositions[player.name] = 0
+            self.player_positions[player.name] = 0
 
         while(self.truns.Size() > 1): 
-            
-            currentPlayer = self.truns.pop()
-            print(f">> {currentPlayer.name}'s chance, press 'r' to roll dice:")
 
-            rollDice = input()
-            
-            if rollDice == "r":
-                diceValue = self.dice.Roll()
-                print(f"{currentPlayer.name} got {diceValue}")
+            current_player = self.truns.pop()
+            print(f">> {current_player.name}'s chance, press 'r' to roll dice:")
 
-                currentPositions = self.playerPositions[currentPlayer.name]
-                nextPosition = currentPositions + diceValue
+            roll_dice = input()
+            
+            if roll_dice == "r":
+                dice_value = self.dice.Roll()
+                print(f"{current_player.name} got {dice_value}")
+
+                current_positions = self.player_positions[current_player.name]
+                next_position = current_positions + dice_value
                 
-                if nextPosition > self.boardSize:
-                    self.truns.put(currentPlayer)
-                    print(f"{currentPlayer.name} is at {currentPositions}")
+                if next_position > self.board_size:
+                    self.truns.put(current_player)
+                    print(f"{current_player.name} is at {current_positions}")
                     pass
-                elif nextPosition == self.boardSize:
-                    print(f"{currentPlayer.name} won the game!")
+                elif next_position == self.board_size:
+                    print(f"{current_player.name} won the game!")
                     return
                 else:
-                    if self.ladders.Check(nextPosition):
-                        self.playerPositions[currentPlayer.name] = self.ladders.Jump(nextPosition)
-                        print(f"[+] {currentPlayer.name} got ladder from {nextPosition} to {self.playerPositions[currentPlayer.name]}")
-                        print(f"{currentPlayer.name} moved to {self.playerPositions[currentPlayer.name]}")
-                    elif self.snakes.Check(nextPosition):
-                        self.playerPositions[currentPlayer.name] = self.snakes.Jump(nextPosition)
-                        print(f"[-] {currentPlayer.name} bitten by snake from {nextPosition} to {self.playerPositions[currentPlayer.name]}")
-                        print(f"{currentPlayer.name} moved to {self.playerPositions[currentPlayer.name]}")
+                    if self.ladders.Check(next_position):
+                        self.player_positions[current_player.name] = self.ladders.Jump(next_position)
+                        print(f"[+] {current_player.name} got ladder from {next_position} to {self.player_positions[current_player.name]}")
+                        print(f"{current_player.name} moved to {self.player_positions[current_player.name]}")
+                    elif self.snakes.Check(next_position):
+                        self.player_positions[current_player.name] = self.snakes.Jump(next_position)
+                        print(f"[-] {current_player.name} bitten by snake from {next_position} to {self.player_positions[current_player.name]}")
+                        print(f"{current_player.name} moved to {self.player_positions[current_player.name]}")
                     else:
-                        self.playerPositions[currentPlayer.name] = nextPosition
-                        print(f"{currentPlayer.name} moved to {nextPosition}")
-                    self.truns.put(currentPlayer)                
+                        self.player_positions[current_player.name] = next_position
+                        print(f"{current_player.name} moved to {next_position}")
+                    self.truns.put(current_player)                
 
                 
             else:
